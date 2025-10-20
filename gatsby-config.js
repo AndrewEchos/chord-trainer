a677fae624cc248b895382b0eb431b1e1d25c50d
Andrew Echos <103871329+AndrewEchos@users.noreply.github.com>
2025-10-20 16:38:58 -0700

    removed deprecated google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,5 @@
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Poppins`,
-            variants: ['400', '700'],
-          },
-          {
-            family: `Open Sans`,
-            variants: [`400`, `700`],
-          },
-        ],
-      },
-    },
     `gatsby-plugin-react-helmet`,
   ],
 }
